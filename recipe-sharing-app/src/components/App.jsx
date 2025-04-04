4846beebff2cdcf5c13f65e6483753ece3b78c6b
nwobodo favour <favourchisom920@gmail.com>
2025-04-04 13:59:05 +0100

    progress

--- recipe-sharing-app/src/components/App.jsx
+++ recipe-sharing-app/src/components/App.jsx
@@ -1,15 +1,14 @@
-
-
+// Import necessary libraries and components
 import React from "react";
-import AddRecipeForm from "./components/AddRecipeForm";  // Correct path to AddRecipeForm component
-import RecipeList from "./components/RecipeList";        // Correct path to RecipeList component
+import AddRecipeForm from "./pages/AddRecipeForm";  // Import the AddRecipeForm component
+import RecipeList from "./pages/RecipeList";        // Import the RecipeList component
 
 function App() {
     return (
         <div>
             <h1>Recipe App</h1>
-            <AddRecipeForm />  {/* Add the form component */}
-            <RecipeList />     {/* Add the list component */}
+            <AddRecipeForm />  {/* Render the AddRecipeForm component */}
+            <RecipeList />     {/* Render the RecipeList component */}
         </div>
     );
 }
