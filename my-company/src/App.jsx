f4587e47269a7164d8af8b8d7a383e1923cbf032
nwobodo favour <favourchisom920@gmail.com>
2025-04-04 13:23:17 +0100

    progress

--- my-company/src/App.jsx
+++ my-company/src/App.jsx
@@ -1,4 +1,14 @@
+
+
+export default App;
+
 import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"; // Import the necessary routing components
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Services from "./pages/Services";
+import Contact from "./pages/Contact";
+import Navbar from "./components/Navbar"; // Optional navbar for navigation
 
 function App() {
     return (
@@ -6,6 +16,17 @@ function App() {
             <h1>Welcome to My React App</h1>
             <p>This is a basic setup.</p>
         </div>
+    );
+    return (
+        <Router>
+            <Navbar /> {/* Navigation bar */}
+            <Routes>
+                <Route path="/" element={<Home />} /> {/* Route for Home page */}
+                <Route path="/about" element={<About />} /> {/* Route for About page */}
+                <Route path="/services" element={<Services />} /> {/* Route for Services page */}
+                <Route path="/contact" element={<Contact />} /> {/* Route for Contact page */}
+            </Routes>
+        </Router>
     );
 }
 
