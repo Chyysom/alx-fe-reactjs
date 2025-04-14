f1a6f75fbfe0dd3311d7c58a835cda0e76e5ea84
nwobodo favour <favourchisom920@gmail.com>
2025-04-14 19:12:58 +0100

    making changes

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,29 +1,20 @@
-import RecipeList from './components/RecipeList'; // Or 
-import AddRecipeForm from './components/AddRecipeForm'; // Or 
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import RecipeDetails from './RecipeDetails';
-import RecipeList from './RecipeList'; // Assuming you have a component to list recipes
+import AddRecipeForm from './components/AddRecipeForm';
+import RecipeList from './components/RecipeList';
+import RecipeDetails from './components/RecipeDetails'; // Make sure the path is correct
 
 function App() {
   return (
     <div>
       <h1>Recipe Sharing App</h1>
       <AddRecipeForm />
-      <RecipeList />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/recipes" element={<RecipeList />} />
+          <Route path="/recipes/:recipeId" element={<RecipeDetails />} />
+        </Routes>
+      </BrowserRouter>
     </div>
-  );
-}
-
-
-
-function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/recipes" element={<RecipeList />} />
-        <Route path="/recipes/:recipeId" element={<RecipeDetails />} />
-      </Routes>
-    </BrowserRouter>
   );
 }
 
